1285f6b6bd296b159b8833a4700869fd62282d29
Aaron G Meininger <a.g.meininger@gmail.com>
2018-05-14 15:57:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,18 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(array) {
   let n = 0
-  while (array) {
-    n++;
-    array[n] + '!!!';
+  while (n < array.length) {
+    
+    array[n++] += '!!!';
   }
   return array
 }
 
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    n++; 
+  }  while (n < 15);
+  return (array)
+}
